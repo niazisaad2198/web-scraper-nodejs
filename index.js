dace63545763921812ff11cdf1c60b23886ab6b8
Muhammad Saad Khan Niazi <niazisaad2198@gmail.com>
2023-04-09 21:48:40 +0500

    ability to fetch html from URL's

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const port = 8000;
+require("dotenv").config();
 
+const axios = require("axios");
 const express = require("express");
 //const cheerio = require("cheerio");
 //const axios = require("axios");
 
 const app = express();
+
+const url = process.env.URL;
+
+axios(url)
+  .then((res) => {
+    const html = res.data;
+    console.log(html);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
